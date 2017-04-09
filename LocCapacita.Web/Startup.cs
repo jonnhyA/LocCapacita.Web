23373656694360038eb60e129aed1cd1b9c0626c
jonnhyA <jonnhy.augusto@gmail.com>
2017-04-09 19:44:28 -0300

    Add project files.

---
+++ LocCapacita.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocCapacita.Web.Startup))]
+namespace LocCapacita.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
